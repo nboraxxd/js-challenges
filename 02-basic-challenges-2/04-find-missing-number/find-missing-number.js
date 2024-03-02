cbf3d717505019136b9d88f46c5029950ad6de25
bora <nhatbaoxxd@gmail.com>
2024-03-02 10:37:56 +0700

    3-4. Find Missing Number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,22 @@
-function findMissingNumber() {}
+/**
+ * Returns the missing number in an array of unique numbers from 1 to n (inclusive).
+ * @param {number[]} arr - The array of numbers.
+ * @returns {number} - The missing number.
+ */
+function findMissingNumber(arr) {
+  if (!Array.isArray(arr) || !arr) return undefined
 
-module.exports = findMissingNumber;
+  if (arr.length === 0) return 1
+
+  const uniqueElements = new Set(arr)
+  if (uniqueElements.size !== arr.length) return undefined
+
+  const n = arr.length + 1
+  const expectedSum = (n * (n + 1)) / 2
+
+  const actualSum = arr.reduce((sum, num) => sum + num, 0)
+
+  return expectedSum - actualSum || undefined
+}
+
+module.exports = findMissingNumber
