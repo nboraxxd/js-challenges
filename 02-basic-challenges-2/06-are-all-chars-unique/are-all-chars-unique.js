fbd5d459aa6ce68fcacef688df49d5cdce5fb843
bora <nhatbaoxxd@gmail.com>
2024-03-03 21:58:10 +0700

    3-6. Are All Chars Unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,46 @@
-function areAllCharactersUnique() {}
+/**
+ * Returns true if all characters in a string are unique.
+ * @param {string} str - The string to check.
+ * @returns {boolean} - Whether all characters in the string are unique.
+ */
+function areAllCharactersUnique(str) {
+  const charCount = {}
 
-module.exports = areAllCharactersUnique;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i]
+
+    if (charCount[char]) return false
+
+    charCount[char] = true
+  }
+
+  return true
+}
+
+// function areAllCharactersUnique(str) {
+//   if (str.length === 0) return true
+
+//   const charSet = new Set()
+
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i]
+//     if (charSet.has(char)) {
+//       return false
+//     }
+
+//     charSet.add(char)
+//   }
+
+//   return true
+// }
+
+// function areAllCharactersUnique(str) {
+//   if (str.length === 0) return true
+
+//   const uniqueElements = new Set(str)
+//   if (uniqueElements.size === str.length) return true
+
+//   return false
+// }
+
+module.exports = areAllCharactersUnique
