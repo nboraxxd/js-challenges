fd0e3e3bba196ab4430c258cbd2cf34055003d43
bora <nhatbaoxxd@gmail.com>
2024-03-03 23:44:46 +0700

    3-7. First Non Repeating Character

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,56 @@
-function findFirstNonRepeatingCharacter() {}
+/**
+ * Returns the first non-repeating character in a string.
+ * @param {string} str - The string to search.
+ * @returns {string | null} - The first non-repeating character in the string or null if there are no non-repeating characters.
+ */
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {}
 
-module.exports = findFirstNonRepeatingCharacter;
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1
+  }
+
+  for (const char of str) {
+    if (charCount[char] === 1) return char
+  }
+
+  return null
+}
+
+// function findFirstNonRepeatingCharacter(str) {
+//   const charCount = {}
+
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i]
+
+//     charCount[char] = charCount[char] ? ++charCount[char] : 1
+//   }
+
+//   for (const key in charCount) {
+//     if (charCount[key] === 1) {
+//       return key
+//     }
+//   }
+
+//   return null
+// }
+
+// function findFirstNonRepeatingCharacter(str) {
+//   const charIndexObject = {}
+
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i]
+
+//     charIndexObject[char] = charIndexObject[char] ? [...charIndexObject[char], i] : [i]
+//   }
+
+//   for (const key in charIndexObject) {
+//     if (charIndexObject[key].length === 1) {
+//       return key
+//     }
+//   }
+
+//   return null
+// }
+
+module.exports = findFirstNonRepeatingCharacter
