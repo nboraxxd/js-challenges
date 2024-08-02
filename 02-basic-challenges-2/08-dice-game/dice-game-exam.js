1e56ce13bf503cb318adb78165ce817486707b54
nboraxxd <nhatbaoxxd@gmail.com>
2024-08-02 09:39:30 +0700

    2-8. dice-game

---
+++ 02-basic-challenges-2/08-dice-game/dice-game-exam.js
@@ -0,0 +1,6 @@
+/**
+ * Simulates the dice game.
+ * @param {number} numSimulations - The number of times to simulate the dice game.
+ * @returns {Array} - An array of simulation result objects.
+ */
+function diceGameSimulation(numSimulations) {}
--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,41 @@
-function diceGameSimulation() {}
+/**
+ * @returns {number} - A random number between 1 and 6.
+ */
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1
+}
 
-module.exports = diceGameSimulation;
+/**
+ * Simulates the dice game.
+ * @param {number} numSimulations - The number of times to simulate the dice game.
+ * @returns {Array} - An array of simulation result objects.
+ */
+function diceGameSimulation(numSimulations) {
+  if (numSimulations <= 0) return []
+
+  const results = []
+
+  Array.from({ length: numSimulations }).forEach(() => {
+    const dice1 = rollDice()
+    const dice2 = rollDice()
+    const sum = dice1 + dice2
+
+    let result = 'roll again'
+    if ([7, 11].includes(sum)) {
+      result = 'win'
+    } else if ([2, 3, 12].includes(sum)) {
+      result = 'lose'
+    }
+
+    results.push({
+      dice1,
+      dice2,
+      sum,
+      result,
+    })
+  })
+
+  return results
+}
+
+module.exports = diceGameSimulation
